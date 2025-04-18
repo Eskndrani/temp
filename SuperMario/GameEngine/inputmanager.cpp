172fb76c8c6164a0975ddc40df4a4ac88abaed70
Fares Osama Abd El-Hameed Elsayed <fares.ossama.24@aucegypt.edu>
2025-04-18 21:00:46 +0200

    Update inputmanager.cpp

--- SuperMario/GameEngine/inputmanager.cpp
+++ SuperMario/GameEngine/inputmanager.cpp
@@ -9,29 +9,91 @@ InputManager::InputManager(QObject *parent)
 
 InputManager::~InputManager()
 {
+    // No dynamic allocations to clean up
 }
 
 void InputManager::processKeyPress(QKeyEvent* event)
 {
-    if (event) {
-        m_pressedKeys.insert(event->key());
+    if (!event) {
+        qWarning() << "Null key event received in processKeyPress";
+        return;
+    }
+    
+    int key = event->key();
+    m_pressedKeys.insert(key);
+    
+    // Emit signal for the corresponding action if mapped
+    if (m_keyToActionMap.contains(key)) {
+        QString action = m_keyToActionMap[key];
+        emit actionTriggered(action, true);
+        
+        // Debug output
+        qDebug() << "Action triggered:" << action;
     }
 }
 
 void InputManager::processKeyRelease(QKeyEvent* event)
 {
-    if (event) {
-        m_pressedKeys.remove(event->key());
+    if (!event) {
+        qWarning() << "Null key event received in processKeyRelease";
+        return;
+    }
+    
+    int key = event->key();
+    m_pressedKeys.remove(key);
+    
+    // Emit signal for the corresponding action if mapped
+    if (m_keyToActionMap.contains(key)) {
+        QString action = m_keyToActionMap[key];
+        emit actionTriggered(action, false);
     }
 }
 
 void InputManager::processMouseInput(QMouseEvent* event)
 {
+    if (!event) {
+        qWarning() << "Null mouse event received";
+        return;
+    }
+    
+    // Store current mouse position
+    m_mousePosition = event->pos();
+    
+    // Process different mouse events
+    switch (event->type()) {
+        case QEvent::MouseButtonPress:
+            emit mouseActionTriggered("click", true, m_mousePosition);
+            break;
+        case QEvent::MouseButtonRelease:
+            emit mouseActionTriggered("click", false, m_mousePosition);
+            break;
+        case QEvent::MouseMove:
+            emit mouseActionTriggered("move", true, m_mousePosition);
+            break;
+        default:
+            break;
+    }
 }
 
 bool InputManager::isKeyPressed(int key) const
 {
     return m_pressedKeys.contains(key);
+}
+
+bool InputManager::isActionActive(const QString& action) const
+{
+    // Check if any key mapped to this action is pressed
+    for (auto it = m_keyToActionMap.begin(); it != m_keyToActionMap.end(); ++it) {
+        if (it.value() == action && m_pressedKeys.contains(it.key())) {
+            return true;
+        }
+    }
+    return false;
+}
+
+QString InputManager::getActionForKey(int key) const
+{
+    return m_keyToActionMap.value(key, "none");
 }
 
 void InputManager::mapControlsToActions()
@@ -47,6 +109,47 @@ void InputManager::mapControlsToActions()
     m_keyToActionMap[Qt::Key_Escape] = "quit";
 }
 
+void InputManager::rebindKey(int oldKey, int newKey)
+{
+    if (m_keyToActionMap.contains(oldKey)) {
+        QString action = m_keyToActionMap[oldKey];
+        m_keyToActionMap.remove(oldKey);
+        m_keyToActionMap[newKey] = action;
+        
+        qDebug() << "Rebound key" << oldKey << "to" << newKey << "for action" << action;
+    }
+}
+
 void InputManager::debounceInput()
 {
+    // Implementation of input debouncing
+    // This prevents rapid firing of the same input
+    
+    // The current implementation uses a simple approach:
+    // We clear and then recreate the pressed keys set
+    // which effectively debounces all inputs
+    
+    m_pressedKeys.clear();
+    
+    // Emit signals to notify that all actions are released
+    for (auto it = m_keyToActionMap.begin(); it != m_keyToActionMap.end(); ++it) {
+        emit actionTriggered(it.value(), false);
+    }
+    
+    qDebug() << "Input debounced - all keys cleared";
+}
+
+void InputManager::clearAllInputs()
+{
+    m_pressedKeys.clear();
+}
+
+QSet<int> InputManager::getPressedKeys() const
+{
+    return m_pressedKeys;
+}
+
+QMap<int, QString> InputManager::getKeyMappings() const
+{
+    return m_keyToActionMap;
 }

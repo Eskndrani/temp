1f7e969055acd0c14783ceb7e18554bff0d0f5ae
Fares Osama Abd El-Hameed Elsayed <fares.ossama.24@aucegypt.edu>
2025-04-18 20:59:17 +0200

    Update inputmanager.h

--- SuperMario/GameEngine/inputmanager.h
+++ SuperMario/GameEngine/inputmanager.h
@@ -3,27 +3,43 @@
 
 #include <QObject>
 #include <QKeyEvent>
+#include <QMouseEvent>
 #include <QSet>
+#include <QMap>
+#include <QPoint>
 
 class InputManager : public QObject
 {
     Q_OBJECT
-    
 public:
-    InputManager(QObject *parent = nullptr);
+    explicit InputManager(QObject *parent = nullptr);
     ~InputManager();
     
     void processKeyPress(QKeyEvent* event);
     void processKeyRelease(QKeyEvent* event);
     void processMouseInput(QMouseEvent* event);
+    
     bool isKeyPressed(int key) const;
+    bool isActionActive(const QString& action) const;
+    QString getActionForKey(int key) const;
+    
+    void rebindKey(int oldKey, int newKey);
+    void debounceInput();
+    void clearAllInputs();
+    
+    QSet<int> getPressedKeys() const;
+    QMap<int, QString> getKeyMappings() const;
+    
+signals:
+    void actionTriggered(const QString& action, bool pressed);
+    void mouseActionTriggered(const QString& action, bool pressed, const QPoint& position);
     
 private:
     void mapControlsToActions();
-    void debounceInput();
     
     QSet<int> m_pressedKeys;
     QMap<int, QString> m_keyToActionMap;
+    QPoint m_mousePosition;
 };
 
-#endif
+#endif // INPUTMANAGER_H
